388c560a7df2636d334a5abcc0abfd8db44e7ba2
Galaxxius <Galaxxius@users.noreply.github.com>
2016-05-13 01:40:10 +0200

    added udp mappings

--- docker_run.sh
+++ docker_run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Set Docker to use the machine
-eval "$(docker-machine env default)"
-
 # Run the server
-docker run -p 28015:28015 -p 28016:28016 -p 8080:8080 -m 4g -v $(pwd)/rust_data:/steamcmd/rust -e RUST_RESPAWN_ON_RESTART=1 --name dockerust -d galaxxius/dockerust:latest
+docker run -p 28015:28015/tcp -p 28015:28015/udp -p 28016:28016 -p 80:8080 -v /steamcmd/rust:/steamcmd/rust -e RUST_RESPAWN_ON_RESTART=1 --name dockerust galaxxius/dockerust:latest
 docker logs -f dockerust
